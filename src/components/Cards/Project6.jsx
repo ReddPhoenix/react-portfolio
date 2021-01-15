010a6726cc51d6343ace1c55ca4ca9a510313e13
Walter Guerrero <wsguerrero@gmail.com>
2021-01-14 17:35:25 -0700

    edited Project 6 description

--- src/components/Cards/Project6.jsx
+++ src/components/Cards/Project6.jsx
@@ -32,12 +32,12 @@ export default function ImgMediaCard() {
             Book vs Movie
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
- The app leverages the Goodreads API and the OMDB API to compare ratings of books and movies to determine if the book is truly always better than the movie. 
+            The app leverages the Goodreads API and the OMDB API to compare ratings of books and movies to determine if the book is truly always better than the movie. (Note: Goodreads is retiring the API after December 2020)
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions style={{display: 'flex', justifyContent: 'center' }}>
-      <Button size="small" color="primary" href='https://uofu-project-1.github.io/Book-vs-Movie/' target='_blank'>
+      <CardActions style={{ display: 'flex', justifyContent: 'center' }}>
+        <Button size="small" color="primary" href='https://uofu-project-1.github.io/Book-vs-Movie/' target='_blank'>
           App
         </Button>
         <Button size="small" color="primary" href='https://github.com/ReddPhoenix/Book-vs-Movie' target='_blank'>
