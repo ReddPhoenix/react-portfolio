2edd182f78d65c351d667adf5f095cca34243cdf
Walter Guerrero <wsguerrero@gmail.com>
2021-04-29 22:54:11 -0600

    applied CSS styling to footer component

--- src/components/HomeNew/TestHome.jsx
+++ src/components/HomeNew/TestHome.jsx
@@ -590,7 +590,7 @@ function TestHome() {
             <div className="footer">
                 <p>
 
-                    <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content is licensed
+                    <a href="http://opensource.org/licenses/mit-license.php">MIT License</a>. The website content is licensed
                         <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
                     </p>
             </div>
