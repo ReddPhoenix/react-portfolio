7d1c7bafc5ffaca8ee4debb4509a1137fc1cc977
Walter Guerrero <wsguerrero@gmail.com>
2021-05-11 18:26:03 -0600

    enabled updated portfolio link

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,19 @@
 import React, {Component} from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
-import Home from './Home/Home';
-// import Portfolio from './Cards/Portfolio.jsx';
-// import Contact from './Contact/Contact';
-import Portfolio from '../../src/components/Cards/Portfolio.jsx';
-import Contact from '../../src/components/Contact/Contact.jsx';
 import ScrollToTop from './ScrollToTop';
-import HomeNew from './HomeNew/HomeNew.jsx';
-import TestHome from './HomeNew/TestHome.jsx';
+import Home from './HomeNew/TestHome.jsx';
 import ScrollToTopButton from '../components/Scroll-To-Top-Button/Scroll-To-Top-Button.jsx';
 import '../components/Scroll-To-Top-Button/Scroll-To-Top-Button.css';
 import GitHubButton from '../components/GitHub-Button/GitHub-Button.jsx';
 import '../components/GitHub-Button/GitHub-Button.css';
 import LinkedInButton from '../components/LinkedIn-Button/LinkedIn-Button.jsx';
 import '../components/LinkedIn-Button/LinkedIn-Button.css';
+// import Home from './Home/Home';
+// import Portfolio from './Cards/Portfolio.jsx';
+// import Contact from './Contact/Contact';
+// import Portfolio from '../../src/components/Cards/Portfolio.jsx';
+// import Contact from '../../src/components/Contact/Contact.jsx';
+// import HomeNew from './HomeNew/HomeNew.jsx';
 
 
 
@@ -26,10 +26,10 @@ class App extends Component {
                     <ScrollToTop />
                     <div >
                         <Route exact path ='/' component = {Home} />
-                        <Route path ='/portfolio' component = {Portfolio} />
+                        {/* <Route path ='/portfolio' component = {Portfolio} />
                         <Route path='/contact' component={Contact} />
                         <Route path='/homenew' component={HomeNew} />
-                        <Route path='/testhome' component={TestHome} />
+                        <Route path='/testhome' component={TestHome} /> */}
                    
                     </div>
                     <LinkedInButton />
